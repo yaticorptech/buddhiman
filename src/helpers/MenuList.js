22650e3e514320d95ab446737179cb3e9a790ad7
kiranCC21 <kiranchoutaje@gmail.com>
2025-07-14 11:48:52 +0530

    menulist update
    updated menulist.js file

--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -3,65 +3,23 @@
 
     export const MenuList = [
         { 
-            name: 'Independence day',
+            name: 'INDEPENDENCE DAY',
             description: ' Celebrate India’s journey of courage, unity, and resilience by participating in our specially curated Independence Day Quiz. Dive into questions about our freedom fighters, national symbols, historical milestones, and constitutional values — and put your knowledge to the test in a fun and rewarding way',
             image: ['/images/ind.png','/images/ind2.png','/images/ind3.png'],
-            Link: '/independence',
+            Link: '/ind',
         },
-        { 
-            name: ' FANCY DRESS',
+            { 
+            name: 'KRISHNA JANMASTAMI',
             description: 'Bring characters to life with your imagination!Contests with fun, festive, and cultural themes.',   
             image: ['/images/ind.png','/images/ind2.png','/images/ind3.png'],
-            Link: '/fancydress',
-        },
+            Link: '/Krishna',
+        }, 
         { 
-            name: 'ART',
-            description: 'Grab your colours and let your imagination run wild!',
-            image: ['/images/ind.png','/images/ind2.png','/images/ind3.png'],
-            Link: '/drawing',
-        },
-        { 
-            name: 'SINGING',
-        description: 'Let your voice shine!Choose from devotional, folk, classical, or film song contests',
-        image: ['/images/ind.png','/images/ind2.png','/images/ind3.png'],
-            Link: '/singing',
-        },
-        { 
-            name: 'DANCE',
-            description:'Lights. Camera. Dance!Whether you twirl in classical or groove in freestyle, here’s your chance to shine.',
-            image: ['/images/ind.png','/images/ind2.png','/images/ind3.png'],
-            Link: '/dance',
-        },
-        { 
-            name: 'Independence day',
-            description: ' Celebrate India’s journey of courage, unity, and resilience by participating in our specially curated Independence Day Quiz. Dive into questions about our freedom fighters, national symbols, historical milestones, and constitutional values — and put your knowledge to the test in a fun and rewarding way',
-            image: ['/images/ind.png','/images/ind2.png','/images/ind3.png'],
-            Link: '/quiz',
-        },
-        { 
-            name: ' FANCY DRESS',
+            name: 'GANESHA CHATURTHI',
             description: 'Bring characters to life with your imagination!Contests with fun, festive, and cultural themes.',   
-        image: ['/images/ind.png','/images/ind2.png','/images/ind3.png'],
-            Link: '/fancydress',
-        },
-        { 
-            name: 'ART',
-            description: 'Grab your colours and let your imagination run wild!',
             image: ['/images/ind.png','/images/ind2.png','/images/ind3.png'],
-            Link: '/drawing',
-        },
-        { 
-            name: 'SINGING',
-        description: 'Let your voice shine!Choose from devotional, folk, classical, or film song contests',
-        image: ['/images/ind.png','/images/ind2.png','/images/ind3.png'],
-            Link: '/singing',
-        },
-        { 
-            name: 'DANCE',
-            description:'Lights. Camera. Dance!Whether you twirl in classical or groove in freestyle, here’s your chance to shine.',
-            image: ['/images/ind.png','/images/ind2.png','/images/ind3.png'],
-            Link: '/dance',
-        },
-        
+            Link: '/Ganesha',
+        }, 
+    
         
     ]; 
