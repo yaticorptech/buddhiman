cae57c00352b6041bcb48ce4aadc7318e090ae8d
kiranCC21 <kiranchoutaje@gmail.com>
2025-07-31 19:51:07 +0530

    menulist.js
    updated content for cards

--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -4,19 +4,19 @@
     export const MenuList = [
         { 
             name: 'INDEPENDENCE DAY',
-            description: ' Celebrate India’s journey of courage, unity, and resilience by participating in our specially curated Independence Day Quiz. Dive into questions about our freedom fighters, national symbols, historical milestones, and constitutional values — and put your knowledge to the test in a fun and rewarding way',
+            description: 'Join our Independence Day Quiz and celebrate India’s journey through fun, facts, and patriotic pride! 🇮🇳',
             image: ['/images/indbanner.jpg','/images/indquiz.jpg','/images/indsing.jpg'],
             Link: '/ind',
         },
             { 
             name: 'KRISHNA JANMASTAMI',
-            description: 'Bring characters to life with your imagination!Contests with fun, festive, and cultural themes.',   
+            description: 'Celebrate Janmashtami with creativity! Join our online contest and let your talent shine. 🌼🦚',   
             image: ['images/krishnabanner.jpg','images/krishnavesha.jpg'],
             Link: '/Krishna',
         }, 
         { 
             name: 'GANESHA CHATURTHI',
-            description: 'Bring characters to life with your imagination!Contests with fun, festive, and cultural themes.',   
+            description: ' Celebrate Ganesh Chaturthi with devotion — join our online contest and showcase your talent for Bappa!🙏✨',   
             image: ['images/ganeshabanner.jpg','images/ganeshaart.jpg','images/ganeshasing.jpg'],
             Link: '/Ganesha',
         }, 
